c9a152959799766c4404b9a06f5639693dd0490a
Adams Anthony Isioma <114524110+AthonysWorld@users.noreply.github.com>
2023-08-09 12:31:10 +0100

    Add files via upload

--- src/components/Best.jsx
+++ src/components/Best.jsx
@@ -1,24 +1,15 @@
 import Card1 from '../images/Card-image1.png';
+import Card2 from '../images/Card-image2.png';
+import Card3 from '../images/Card-image3.png';
 
 const Best = () => {
   return (
-    <div>
-      <img src={Card1}/>
-    </div>
-  )
-}
-
-export default Best
-
-
-
-
-{/* <div className='best-container'>
-<span className='colored'><h6>BEST SERVICES</h6></span>
+    <div  className='best-container'>
+    <span className='colored'><h6>BEST SERVICES</h6></span>
  <h1>Most Popular Car Rental Deals</h1>
  <p>Explore below our best car services experience like never before</p>
 
-<div className='container-fluid row best-container'>
+ <div className='container-fluid row best-container'>
 <div className='card col-lg-4'>
 <img src={Card1}/>
 <h5>Toyota Corolla T-20</h5>
@@ -37,6 +28,7 @@ export default Best
 <button className='btn cardBtn'>Rent now</button>
    </div>
 
+
    <div className='card col-lg-4'>
    <img src={Card3}/>
 <h5>Toyota Corolla T-20</h5>
@@ -44,5 +36,14 @@ export default Best
 <span><input type='checkbox' checked/> Bluetooth Sound System</span>
 <span><input type='checkbox' checked/> Sunroof Available</span>
 <button className='btn cardBtn'>Rent now</button>
-   </div> */}
+   </div>
+
+
+</div>
+    </div>
+  )
+}
+
+export default Best
+
 

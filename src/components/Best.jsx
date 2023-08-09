6296965b6183af25dad65bf0518dbe7df2a760e9
Adams Anthony Isioma <114524110+AthonysWorld@users.noreply.github.com>
2023-08-09 12:19:03 +0100

    Add files via upload

--- src/components/Best.jsx
+++ src/components/Best.jsx
@@ -1,48 +1,48 @@
-
-// import Card1 from '../images/card-image1.png'
-import Card2 from '../images/card-image2.png'
-import Card3 from '../images/card-image3.png'
+import Card1 from '../images/Card-image1.png';
 
 const Best = () => {
   return (
-    <div className='best-container'>
-     <span className='colored'><h6>BEST SERVICES</h6></span>
-      <h1>Most Popular Car Rental Deals</h1>
-      <p>Explore below our best car services experience like never before</p>
-     
-     <div className='container-fluid row best-container'>
-     <div className='card col-lg-4'>
-     {/* <img src={Card1}/> */}
-<h5>Toyota Corolla T-20</h5>
-<span><input type='checkbox' checked/> Air Conditioned</span>
-<span><input type='checkbox' checked/> Bluetooth Sound System</span>
-<span><input type='checkbox' checked/> Sunroof Available</span>
-<button className='btn cardBtn'>Rent now</button>
-     </div>
-
-     <div className='card col-lg-4'>
-     <img src={Card2}/>
-<h5>Toyota Corolla T-20</h5>
-<span><input type='checkbox' checked/> Air Conditioned</span>
-<span><input type='checkbox' checked/> Bluetooth Sound System</span>
-<span><input type='checkbox' checked/> Sunroof Available</span>
-<button className='btn cardBtn'>Rent now</button>
-        </div>
-
-        <div className='card col-lg-4'>
-        <img src={Card3}/>
-<h5>Toyota Corolla T-20</h5>
-<span><input type='checkbox' checked/> Air Conditioned</span>
-<span><input type='checkbox' checked/> Bluetooth Sound System</span>
-<span><input type='checkbox' checked/> Sunroof Available</span>
-<button className='btn cardBtn'>Rent now</button>
-        </div>
-
-
-     </div>
-      
+    <div>
+      <img src={Card1}/>
     </div>
   )
 }
 
 export default Best
+
+
+
+
+{/* <div className='best-container'>
+<span className='colored'><h6>BEST SERVICES</h6></span>
+ <h1>Most Popular Car Rental Deals</h1>
+ <p>Explore below our best car services experience like never before</p>
+
+<div className='container-fluid row best-container'>
+<div className='card col-lg-4'>
+<img src={Card1}/>
+<h5>Toyota Corolla T-20</h5>
+<span><input type='checkbox' checked/> Air Conditioned</span>
+<span><input type='checkbox' checked/> Bluetooth Sound System</span>
+<span><input type='checkbox' checked/> Sunroof Available</span>
+<button className='btn cardBtn'>Rent now</button>
+</div>
+
+<div className='card col-lg-4'>
+<img src={Card2}/>
+<h5>Toyota Corolla T-20</h5>
+<span><input type='checkbox' checked/> Air Conditioned</span>
+<span><input type='checkbox' checked/> Bluetooth Sound System</span>
+<span><input type='checkbox' checked/> Sunroof Available</span>
+<button className='btn cardBtn'>Rent now</button>
+   </div>
+
+   <div className='card col-lg-4'>
+   <img src={Card3}/>
+<h5>Toyota Corolla T-20</h5>
+<span><input type='checkbox' checked/> Air Conditioned</span>
+<span><input type='checkbox' checked/> Bluetooth Sound System</span>
+<span><input type='checkbox' checked/> Sunroof Available</span>
+<button className='btn cardBtn'>Rent now</button>
+   </div> */}
+
